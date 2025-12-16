374f1c536feee978f1f7c7400e9dd0decee890d5
Johannes Grimm <grimmjohannes1998@gmail.com>
2025-12-16 11:17:13 +0100

    Add Attendee View

--- app/dashboard/attendees/page.tsx
+++ app/dashboard/attendees/page.tsx
@@ -1,5 +1,16 @@
+import {fetchStudentsPage} from "@/lib/data";
 
-export default async function Page() {
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchStudentsPage(query);
+
     return (
         <>
             Attendees
---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'prefer'});
+
+export async function fetchStudentsPage(query: string) {
+    try {
+        const data = await sql<[]>`SELECT * FROM students`;
+        return data;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error("Failed to fetch students.\n"+ error);
+    }
+}
